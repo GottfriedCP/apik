78d898519984acac43eacd13192bb27c14194745
unknown <gottfriedcpn@gmail.com>
2024-07-03 23:16:37 +0700

    use htmx for long task

--- apik/urls.py
+++ apik/urls.py
@@ -1,16 +1,29 @@
-from django.urls import path
+from django.urls import include, path
 
-from . import views
+from . import views, views_htmx
 
-app_name = 'apik'
+app_name = "apik"
+
+htmx_urlpatterns = [
+    path("get-index-table/", views_htmx.get_index_table, name="get_index_table"),
+    path(
+        "get-eligible-imuns-count/",
+        views_htmx.get_eligible_imuns_count,
+        name="get_eligible_imuns_count",
+    ),
+]
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-
-    path('detail-bayi/<int:bayi_id>/', views.bayi_detail, name='bayi_detail'),
-    path('tambah-imunisasi/', views.tambah_imunisasi, name='tambah_imunisasi'),
-    path('konfirmasi-hapus/', views.konfirmasi_hapus_imun_d, name='konfirmasi_hapus_imun_d'),
-    path('hapus/', views.hapus_imun_d, name='hapus_imun_d'),
+    path("", views.index, name="index"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("detail-bayi/<int:bayi_id>/", views.bayi_detail, name="bayi_detail"),
+    path("tambah-imunisasi/", views.tambah_imunisasi, name="tambah_imunisasi"),
+    path(
+        "konfirmasi-hapus/",
+        views.konfirmasi_hapus_imun_d,
+        name="konfirmasi_hapus_imun_d",
+    ),
+    path("hapus/", views.hapus_imun_d, name="hapus_imun_d"),
+    path("htmx/", include(htmx_urlpatterns)),
 ]
---
+++ apik/views_htmx.py
@@ -0,0 +1,80 @@
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseForbidden
+from django.shortcuts import render
+from django.utils import timezone
+
+from dateutil.relativedelta import relativedelta
+
+from .models import Bayi, Bidan, Ibu, Imunisasi, ImunisasiDiberikan
+
+
+@login_required
+def get_index_table(request):
+    try:
+        nik_session = request.session["apik_nik"]
+    except KeyError:
+        return HttpResponseForbidden("Mohon logout dahulu via situs Administrasi APIK.")
+    balitas = Bayi.objects.select_related("ibu").prefetch_related("bidans")
+    if request.user.groups.filter(name="ibu").exists():
+        # user adalah ibu
+        ibu = Ibu.objects.get(nik=nik_session)
+        balitas = balitas.filter(ibu=ibu)
+    else:
+        # user adalah bidan
+        balitas = balitas.all()
+        # exclude anak di atas 5 tahun
+        date_59_months_ago = timezone.now().date() - relativedelta(months=59)
+        balitas = balitas.filter(tanggal_lahir__gte=date_59_months_ago)
+
+    eligible_imuns_count_list = []
+    imunisasis = Imunisasi.objects.prefetch_related("bayis")
+    for balita in balitas:
+        # imunisasis = Imunisasi.objects.prefetch_related("bayis")
+        # list imunisasi yang belum diberikan
+        eligible_imuns = imunisasis.exclude(bayis__in=(balita,))
+        # list imunisasi yang memenuhi syarat usia dari query above
+        eligible_imuns = eligible_imuns.filter(syarat_usia__lt=balita.get_usia_bulan())
+        eligible_imuns_count_list.append(eligible_imuns.count())
+    balitas = zip(balitas, eligible_imuns_count_list)
+
+    return render(
+        request,
+        "apik/htmx/index_table.html",
+        {
+            "balitas": balitas,
+        },
+    )
+
+
+def get_eligible_imuns_count(request):
+    print("Hitung total dosis imunisasi yang bisa diberikan...")
+
+    # exclude anak di atas 5 tahun
+    date_59_months_ago = timezone.now().date() - relativedelta(months=59)
+    balitas = Bayi.objects.filter(tanggal_lahir__gte=date_59_months_ago)
+
+    imunisasis = Imunisasi.objects.prefetch_related("bayis")
+
+    eligible_imuns_count = 0
+
+    for balita in balitas:
+        # 1. imunisasi yg bisa diberikan ke anak ini
+        eligible_imuns = imunisasis.filter(syarat_usia__lt=balita.get_usia_bulan())
+        # 2. exclude imunisasi yg sudah diberikan ke anak ini
+        # {% if imun_e.maksimum_usia and balita.get_usia_bulan >= imun_e.maksimum_usia %}
+        eligible_imuns = eligible_imuns.exclude(bayis__in=(balita,))
+        for eligible_imun in eligible_imuns:
+            if eligible_imun.maksimum_usia:
+                if balita.get_usia_bulan() < eligible_imun.maksimum_usia:
+                    eligible_imuns_count += 1
+            else:
+                eligible_imuns_count += 1
+        print(f"selesai 1 balita: eligible {eligible_imuns_count} imunisasi")
+    print(f"Total {eligible_imuns_count} dosis imunisasi bisa diberikan")
+
+    return render(
+        request,
+        "apik/htmx/eligible_imuns_count.html",
+        {"eligible_imuns_count": eligible_imuns_count},
+    )
